1dce95ca93a4186d4879d1f1bf06b562812e68f6
syall <s.yuan.all@gmail.com>
2020-02-18 19:57:31 -0500

    Refactor and add comments

--- utils.js
+++ utils.js
@@ -1,3 +1,7 @@
+/**
+ * Configuratin
+ */
+
 function initConfig() {
     const button = document.getElementById('toggle-fs');
     button.addEventListener('click', toggleFs);
@@ -47,4 +51,33 @@ function toggleFs() {
     return (notFullScreen)
         ? requestFullScreen.call(docEl)
         : cancelFullScreen.call(doc);
+}
+
+/**
+ * Animation
+ */
+
+function scene(frames) {
+    for (const [t, fn] of frames) fn(t);
+}
+
+function blink(t) {
+    const { ctx, height, width } = world;
+    ctx.clearRect(0, 0, width, height);
+    ctx.fillStyle = "#000000";
+    setTimeout(() => ctx.fillRect(0, 0, width, height), t);
+    clear(t + 250);
+    setTimeout(() => ctx.fillRect(0, 0, width, height), t + 400);
+    clear(t + 650);
+}
+
+function wakeUp(t) {
+    const { ctx, height, width } = world;
+    ctx.textAlign = "center";
+    setTimeout(() => ctx.fillText("WAKE UP.", width / 2, height / 2), t);
+}
+
+function clear(t) {
+    const { ctx, height, width } = world;
+    setTimeout(() => ctx.clearRect(0, 0, width, height), t);
 }

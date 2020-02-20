19f9710f7f7038ab72c4f858636bc140f618e316
syall <s.yuan.all@gmail.com>
2020-02-20 00:03:58 -0500

    Add no repeat if local storage true

--- index.js
+++ index.js
@@ -5,19 +5,22 @@ initConfig();
 const world = initWorld();
 
 // Opening Scene
-scene([
-    [0000, text('WARNING: GAME CAN CAUSE SEIZURES')],
-    [3000, blink()()],
-    [3400, blink()()],
-    [4000, text('WAKE UP.')],
-    [6000, blink()()],
-    [6400, blink()()],
-    [7000, text('DO YOU KNOW HOW LONG IT\'S BEEN.')],
-    [9000, blink()()],
-    [9400, blink()()],
-    [9999, initDraw],
-    // [0000, initDraw], // Debug
-]);
+(window.localStorage['way-to-go-opening'])
+    ? initDraw(0)
+    : scene([
+        [0000, text('WARNING: GAME CAN CAUSE SEIZURES')],
+        [3000, blink()()],
+        [3400, blink()()],
+        [4000, text('WAKE UP.')],
+        [6000, blink()()],
+        [6400, blink()()],
+        [7000, text('DO YOU KNOW HOW LONG IT\'S BEEN.')],
+        [9000, blink()()],
+        [9400, blink()()],
+        [9999, initDraw],
+        [0000, () => window.localStorage['way-to-go-opening'] = true]
+    ]);
+
 
 // Add Player to World
 const p = { pos: { y: 30, x: 60 }, keys: new Map() };

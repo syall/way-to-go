b3146b3998078e1b38e0b99fed1d832c9239f731
syall <s.yuan.all@gmail.com>
2020-02-20 18:12:58 -0500

    Add Unicode along with Player Direction

--- utils.js
+++ utils.js
@@ -2,6 +2,7 @@
  * Configuration
  */
 
+// Configuration: FullScreen
 function initConfig() {
 
     // Toggle Full Screen Button
@@ -50,6 +51,7 @@ function initConfig() {
 
 }
 
+// Initialize World
 function initWorld() {
 
     // High DPI Canvas
@@ -83,90 +85,134 @@ function initWorld() {
     };
 }
 
+// Initialize Dungeon
 function initDungeon() {
-    const max_rooms = 100;
-    const min_size = 10;
-    const max_size = 20;
 
+    // Get Stored Grid
+    if (sessionStorage.getItem('grid')) {
+        world.grid = JSON.parse(sessionStorage.getItem('grid'));
+        return;
+    }
+
+    // Max Number of Iterations
+    const max = 100_000;
+    // Minimum Length of Room
+    const min_size = 8;
+    // Maximum Size of Room
+    const max_size = 12;
+    // Height of Grid
+    const gh = world.height / 10 - 1;
+    // Width of Grid
+    const gw = world.width / 10 - 1;
+
+    // Add Rooms
     addToWorld('rooms', [], () => null);
-    for (let i = 0; i < max_rooms; i++) {
-        const gh = world.height / 10 - 1;
-        const gw = world.width / 10 - 1;
-        const y = rng(1, gh);
-        const x = rng(1, gw);
-        const w = rng(min_size, max_size);
-        const h = rng(min_size, max_size);
-        if (y + h > gh || x + w > gw) {
-            i--;
+
+    // Generate Rooms
+    for (let i = 0; i < max; i++) {
+
+        // Top Left Corner
+        const y = rng(0, gh);
+        const x = rng(0, gw);
+
+        // Width and Height
+        let w = rng(min_size, max_size);
+        let h = rng(min_size, max_size);
+
+        // If out of bounds, ignore
+        if (y + h > gh || x + w > gw)
             continue;
-        }
+
+        // Create Room
         const cur = { w, h, y, x };
+
+        // If there is an intersection, ignore
         if (world.rooms.some(r => intersect(r, cur)))
             continue;
+
+        // Add Room
         addRoom(cur);
-        if (world.rooms.length > 1) {
+
+        // Tunnel if Another Room Exists
+        if (world.rooms.length !== 0) {
+
+            // Center of Current Room
             const cy = Math.floor(cur.y + (cur.h / 2));
             const cx = Math.floor(cur.x + (cur.w / 2));
-            const prev = world.rooms[world.rooms.length - 2];
+
+            // Center of Previous Room
+            const prev = world.rooms[world.rooms.length - 1];
             const py = Math.floor(prev.y + (prev.h / 2));
             const px = Math.floor(prev.x + (prev.w / 2));
+
+            // Horizontal First
             if (rng(0, 1) === 1) {
                 hor_tunnel(px, cx, py);
                 ver_tunnel(py, cy, cx);
-            } else {
+            }
+            // Vertical First
+            else {
                 ver_tunnel(py, cy, px);
                 hor_tunnel(px, cx, cy);
             }
         }
+
+        // Add Room to World
+        world.rooms.push(cur);
     }
 
-    // Add Player to World
+    // Add Player
     const { w, h, y, x } = world.rooms[0];
-    const cy = Math.floor(y + (h / 2));
-    const cx = Math.floor(x + (w / 2));
     const p = {
         pos: {
-            y: cy,
-            x: cx
+            y: Math.floor(y + (h / 2)),
+            x: Math.floor(x + (w / 2))
         },
         keys: new Map()
     };
     addToWorld('player', p, () => {
-        world.grid[p.pos.y][p.pos.x] = tile.player;
+        world.grid[p.pos.y][p.pos.x] = tile.def;
         player_keys();
     });
 
+    // Add to session storage
+    sessionStorage.setItem('grid', JSON.stringify(world.grid));
+    sessionStorage.removeItem('grid');
+
+    // Room Intersection
+    function intersect(r1, r2) {
+        return (
+            r1.x <= r2.x + r2.w &&
+            r2.x + r2.w >= r2.x &&
+            r1.y <= r2.y + r2.h &&
+            r1.y + r1.h >= r2.y
+        );
+    }
+
+    // Apply Room
+    function addRoom(room) {
+        for (let i = room.y; i < room.y + room.h; i++)
+            for (let j = room.x; j < room.x + room.w; j++)
+                world.grid[i][j] = tile.floor;
+    }
+
+    // Apply Horizontal Tunnel
+    function hor_tunnel(px, cx, py) {
+        for (let i = Math.min(px, cx); i <= Math.max(px, cx); i++)
+            world.grid[py][i] = tile.floor;
+    }
+
+    // Apply Vertical Tunnel
+    function ver_tunnel(py, cy, px) {
+        for (let i = Math.min(py, cy); i <= Math.max(py, cy); i++)
+            world.grid[i][px] = tile.floor;
+    }
+
 }
 
-// Random Numbers: l to h
+// Random Numbers: l to h inclusive, [l, h]
 function rng(l, h) {
     return Math.floor(Math.random() * (h - l)) + l + 1;
-}
-
-function intersect(r1, r2) {
-    return (
-        r1.x <= r2.x + r2.w &&
-        r2.x + r2.w >= r2.x &&
-        r1.y <= r2.y + r2.h &&
-        r1.y + r1.h >= r2.y
-    );
-}
-
-function addRoom(room) {
-    for (let i = room.y; i < room.y + room.h; i++)
-        for (let j = room.x; j < room.x + room.w; j++)
-            world.grid[i][j] = tile.floor;
-    world.rooms.push(room);
-}
-
-function hor_tunnel(px, cx, py) {
-    for (let i = Math.min(px, cx); i <= Math.max(px, cx); i++)
-        world.grid[py][i] = tile.floor;
-}
-
-function ver_tunnel(py, cy, px) {
-    for (let i = Math.min(py, cy); i <= Math.max(py, cy); i++)
-        world.grid[i][px] = tile.floor;
 }
 
 /**
@@ -226,10 +272,11 @@ function initDraw(t) {
 
 // Tile Enumeration
 const tile = {
-    wall: '■',
-    floor: ' ',
-    trail: '□',
-    player: '@'
+    wall: '',
+    floor: '◻', // '□',
+    trail: '◼', // '⊡', // '■',
+    player: '',
+    def: '⽅', // '⏣'
 };
 
 // Draw Grid
@@ -284,19 +331,25 @@ const player_keys = () => {
 
         if (world.player.keys.get(dirKeys.LEFT, true) && !block(y)(x - 1)) {
             world.player.pos.x -= 1;
+            tile.player = '左'; // '←';
             e.preventDefault();
         }
-        if (world.player.keys.get(dirKeys.RIGHT, true) && !block(y)(x + 1)) {
+        else if (world.player.keys.get(dirKeys.RIGHT, true) && !block(y)(x + 1)) {
             world.player.pos.x += 1;
+            tile.player = '右'; // '→';
             e.preventDefault();
         }
-        if (world.player.keys.get(dirKeys.UP, true) && !block(y - 1)(x)) {
+        else if (world.player.keys.get(dirKeys.UP, true) && !block(y - 1)(x)) {
             world.player.pos.y -= 1;
+            tile.player = '上'; // '↑';
             e.preventDefault();
         }
-        if (world.player.keys.get(dirKeys.DOWN, true) && !block(y + 1)(x)) {
+        else if (world.player.keys.get(dirKeys.DOWN, true) && !block(y + 1)(x)) {
             world.player.pos.y += 1;
+            tile.player = '下'; // '↓';
             e.preventDefault();
+        } else {
+            tile.player = tile.def;
         }
 
         world.grid[y][x] = tile.trail;
@@ -304,7 +357,11 @@ const player_keys = () => {
         drawWorld();
     };
 
-    const keysReleased = e => world.player.keys.set(e.keyCode, false);
+    const keysReleased = e => {
+        world.player.keys.set(e.keyCode, false);
+        world.grid[world.player.pos.y][world.player.pos.x] = tile.def;
+        drawWorld();
+    };
 
     window.addEventListener('keydown', keysPressed, false);
     window.addEventListener('keyup', keysReleased, false);
